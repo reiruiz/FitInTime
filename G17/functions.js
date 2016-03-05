4621f0c7334bdd6d42c1036fd92b5f35414d4bd6
Christopher Centenera <Christopher_Centenera@hotmail.com>
2016-03-05 11:47:04 -0800

    Added id's to login and registration forms, validation will be simple for now while debugging. Adding highlighting field feature as a general function and feedback text in next push.

--- G17/functions.js
+++ G17/functions.js
@@ -1,15 +1,31 @@
-function validateNewUser(){
+
+function $(id){
+    var element = document.getElementById(id);
+    if(element == null){
+        alert("Error at the programming level");
+    }
+    return element;
+}
+function errorMsg(msg, childId, parentId){
+    var error = document.createElement("p");
+    var text = document.createTextNode(msg);
+    error.appendChild(text);
+    parentId.insertBefore(error,childId);
+}
+function validateNewUser(id){
     //Gets string for new user name
-    var user = document.getElementById("regusername").value;
-    //Test pattern for user
+    var element = $(id);
+    //Username must be alphanumeric
     var patt = /\W/;
-    if(patt.test(user)){
-        alert("Invalid new username");
+    if(patt.test(element.value)){
+        element.style.backgroundColor = "Pink";
+        element.style.border = "1px solid red";
+        errorMsg("Invalid user name.", element, element.parentNode);
     }
 }
-function validateNewPass(){
+function validateNewPass(id){
     //Gets string for password
-    var pass = document.getElementById("regpassword").value;
+    var pass = $(id).value;
     //Builds a string containing what user is missing in password
     var errorString = "";
     //Password must contain uppercase letter, lowercase letter and a number
@@ -27,5 +43,23 @@ function validateNewPass(){
     }
     if(errorString.length > 0) {
         alert(errorString);
+    }
+}
+
+function validateEmail(id){
+    var email = $(id).value;
+    /*
+    pattern matches to format:
+    [any alphanumeric characters including underscore]@[any domain].[toplevel domain(min 2 char)]
+    */
+    var patt = /[\w_]*@.*\.\a{2,}/;
+    if(!patt.test(email)){
+        alert("Invalid email");
+    }
+}
+
+function confirmInput(original, confirmer){
+    if($(original).value != $(confirmer).value){
+        alert("Original does not match confirmation.");
     }
 }
